21503c4c2e85ee790882bcd4c9c95bf2b318516e
shourok-abdullah <shourok181330@feng.bu.edu.eg>
2022-10-08 14:30:48 +0200

    dep

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://www.ticketin.me/',
+      baseURL: 'http://www.ticketin.me',
       headers: req.headers,
     })
   } else {
