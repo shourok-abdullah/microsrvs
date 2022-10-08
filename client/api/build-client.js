0a91456a4444d10cd5aed33805ae772c73ff3264
shourok-abdullah <shourok181330@feng.bu.edu.eg>
2022-10-08 14:26:50 +0200

    dep

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,20 +1,19 @@
-import axios from "axios";
+import axios from 'axios'
 
 const buildClient = ({ req }) => {
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        'http://www.ticketing-me.live',
+      baseURL: 'http://www.ticketin.me/',
       headers: req.headers,
-    });
+    })
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: "/",
-    });
+      baseUrl: '/',
+    })
   }
-};
+}
 
-export default buildClient;
+export default buildClient
