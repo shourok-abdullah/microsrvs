f9464b22ce4659da2e743fed8bb05435dca365d5
shourok-abdullah <shourok181330@feng.bu.edu.eg>
2022-10-08 13:42:02 +0200

    dep

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
-import { useState } from 'react';
+import axios from 'axios'
+import { useState } from 'react'
 
 const useRequest = ({ url, method, body, onSuccess }) => {
-  const [errors, setErrors] = useState(null);
+  const [errors, setErrors] = useState(null)
 
   const doRequest = async (props = {}) => {
     try {
-      setErrors(null);
-      const response = await axios[method](url, { ...body, ...props });
+      setErrors(null)
+      const response = await axios[method](url, { ...body, ...props })
 
       if (onSuccess) {
-        onSuccess(response.data);
+        onSuccess(response.data)
       }
 
-      return response.data;
+      return response.data
     } catch (err) {
       setErrors(
         <div className="alert alert-danger">
@@ -23,12 +23,12 @@ const useRequest = ({ url, method, body, onSuccess }) => {
               <li key={err.message}>{err.message}</li>
             ))}
           </ul>
-        </div>
-      );
+        </div>,
+      )
     }
-  };
+  }
 
-  return { doRequest, errors };
-};
+  return { doRequest, errors }
+}
 
-export default useRequest;
+export default useRequest
