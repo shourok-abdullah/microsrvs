4802fd80c31689eb324a076c4a0e31e6215f54d0
shourok-abdullah <shourok181330@feng.bu.edu.eg>
2022-10-08 13:04:06 +0200

    Revert "dep"
    This reverts commit 4f650f4ad577603a184cf254d449eeff12d26589.

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,34 +1,34 @@
-import axios from 'axios'
-import { useState } from 'react'
+import axios from 'axios';
+import { useState } from 'react';
 
 const useRequest = ({ url, method, body, onSuccess }) => {
-  const [errors, setErrors] = useState(null)
+  const [errors, setErrors] = useState(null);
 
   const doRequest = async (props = {}) => {
     try {
-      setErrors(null)
-      const response = await axios[method](url, { ...body, ...props })
+      setErrors(null);
+      const response = await axios[method](url, { ...body, ...props });
 
       if (onSuccess) {
-        onSuccess(response.data)
+        onSuccess(response.data);
       }
 
-      return response.data
+      return response.data;
     } catch (err) {
       setErrors(
         <div className="alert alert-danger">
           <h4>Ooops....</h4>
           <ul className="my-0">
-            {err.response.data.errors?.map((err) => (
+            {err.response.data.errors.map((err) => (
               <li key={err.message}>{err.message}</li>
             ))}
           </ul>
-        </div>,
-      )
+        </div>
+      );
     }
-  }
+  };
 
-  return { doRequest, errors }
-}
+  return { doRequest, errors };
+};
 
-export default useRequest
+export default useRequest;
